a181b60e06c6cabe95ab461603242cd8ff51c2b2
luanspataro <luan.spataro@hotmail.com>
2024-02-12 01:40:10 -0300

    Projeto Concluído.

---
+++ T2S/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace T2S.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
